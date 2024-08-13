ffbecf0076274fd27ae1413fd75b5d6b9fdafb62
Agus <agussmkertjhaan@gmail.com>
2024-08-13 17:00:13 +0700

    feat: new ui

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -14,7 +14,7 @@ const { data: articles } = await useAsyncData("all-articles", () =>
 
 <template>
   <main class="min-h-screen">
-    <AppHeader class="mb-16" title="Articles" :description="description" />
+    <AppArticleHeader class="mb-16" title="Articles" :description="description" />
     <ul class="space-y-16">
       <li v-for="(article, id) in articles" :key="id">
         <AppArticleCard :article="article" />
