1d149bb466a26ef9228c3a2f191882f12d3310f4
Agus SM <mrschwartx@gmail.com>
2025-04-03 18:13:34 +0700

    feat: update code

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    target: 'esnext', 
-  },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+    }),
+  ],
 })
